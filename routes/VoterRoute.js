39deeff68661a6b1149380b803f5f67765de954b
collinstb01 <collinstb01@gmail.com>
2023-11-21 10:48:58 +0100

    function name fixed

--- routes/VoterRoute.js
+++ routes/VoterRoute.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const {
-  _sigUpnUser,
-  _loginUser,
+  signUpUser,
+  loginUser,
 } = require("../controllers/voter/AuthController");
 
 const voterRoute = express.Router();
 
-voterRoute.post("/sign-up", _sigUpnUser);
-voterRoute.post("/sign-in", _loginUser);
+voterRoute.post("/sign-up", signUpUser);
+voterRoute.post("/sign-in", loginUser);
 
 module.exports = voterRoute;
