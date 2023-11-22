02138fa7c27c4584afe08d8fe403e7c82db48fc4
collinstb01 <collinstb01@gmail.com>
2023-11-22 05:28:19 +0100

    added home route for API

--- routes/VoterRoute.js
+++ routes/VoterRoute.js
@@ -5,8 +5,13 @@ const {
 } = require("../controllers/voter/AuthController");
 
 const voterRoute = express.Router();
+const app = express();
 
 voterRoute.post("/sign-up", signUpUser);
 voterRoute.post("/sign-in", loginUser);
+
+app.get("/", (req, res) => {
+  res.send("App Running");
+});
 
 module.exports = voterRoute;
